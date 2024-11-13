81587320f5e9f643a81f4e16d95499f66986df7f
gjuliandev <gjulian.dev@gmail.com>
2024-11-13 10:26:41 +0100

    wip solicitud

--- src/routes/solicitud.route.ts
+++ src/routes/solicitud.route.ts
@@ -1,17 +1,27 @@
 import { Router } from "express";
-import { deleteSolicitud, getSolicitud, getSolicitudesByContenedor, getSolicitudesByEmpleado, getStatusSolicitud, getTiposSolicitud, postSolicitud, rechazarSolicitud, validarSolicitud } from "../controllers/solicitudes.controller";
+import {
+  deleteSolicitud,
+  getSolicitud,
+  getSolicitudesByContenedor,
+  getSolicitudesByEmpleado,
+  getStatusSolicitud,
+  getTiposSolicitud,
+  postSolicitud,
+  rechazarSolicitud,
+  validarSolicitud,
+} from "../controllers/solicitudes.controller";
 
 const routes = Router();
 
+routes.get("/status-solicitud", getStatusSolicitud);
+routes.get("/tipos-solicitud", getTiposSolicitud);
 routes.get("/:contenedor_id/contenedor", getSolicitudesByContenedor);
 routes.get("/:empleado_id/empleado", getSolicitudesByEmpleado);
 routes.get("/:solicitud_id", getSolicitud);
-routes.get('/status-solicitud', getStatusSolicitud);
-routes.get('/tipos-solicitud', getTiposSolicitud)
+
 routes.post("/", postSolicitud);
 routes.patch("/:solicitud_id/validar", validarSolicitud);
 routes.patch("/:solicitud_id/rechazar", rechazarSolicitud);
 routes.delete("/:solicitud_id", deleteSolicitud);
-
 
 export default routes;
