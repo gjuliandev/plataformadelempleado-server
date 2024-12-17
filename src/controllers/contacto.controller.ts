993a094479173111d97886f78ae418ccad94ee83
gjuliandev <gjulian.dev@gmail.com>
2024-12-17 18:55:23 +0100

    fix

--- src/controllers/contacto.controller.ts
+++ src/controllers/contacto.controller.ts
@@ -1,126 +1,114 @@
-import { Request, Response } from 'express';
-import MySql from '../db/mysql';
+import { Request, Response } from "express";
+import MySql from "../db/mysql";
 
-export const getContactosByCliente = (req: Request, res: Response ) =>  {
+export const getContactosByCliente = (req: Request, res: Response) => {
+  const { idDireccion } = req.params;
 
-    const { idDireccion } = req.params;
+  const query = `SELECT * FROM contactos WHERE cliente_id = ${idDireccion}`;
 
-    const query = `SELECT * FROM contactos WHERE cliente_id = ${idDireccion}`;
+  MySql.ejecutarQuery(query, [], (err: any, contacto: any) => {
+    if (err) {
+      return res.status(400).json({
+        msg: err,
+      });
+    }
 
-    MySql.ejecutarQuery(query, [], (err: any, contacto: any) => {
-        if ( err ) {
-            return res.status(400).json({
-                msg: err
-            });
-        }
-
-        res.status(200).json({
-            payload: contacto
-        });
+    res.status(200).json({
+      payload: contacto,
     });
-}
+  });
+};
 
-export const postContacto= (req: Request, res: Response ) =>  {
+export const postContacto = (req: Request, res: Response) => {
+  const { body } = req;
 
-    const { body } = req;
+  const query = "INSERT INTO contactos (nombre, puesto, email, telefono, web, skype, twiter, facebook, instagram, cliente_id) " + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  const campos = [body.nombre, body.puesto, body.email, body.telefono, body.web, body.skype, body.twiter, body.facebook, body.instagram, body.cliente_id];
 
+  MySql.ejecutarQuery(query, campos, (err: any, result: any) => {
+    if (err) {
+      return res.status(400).json({
+        msg: err,
+      });
+    }
 
-    const query = 'INSERT INTO contactos (nombre, puesto, email, telefono, web, skype, twiter, facebook, instagram, cliente_id) ' +
-                   ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    const campos = [body.nombre, body.puesto, body.email, body.telefono, body.web, body.skype, body.twiter, body.facebook, body.instagram, body.cliente_id ];
-
-    MySql.ejecutarQuery(query, campos, (err: any, result: any) => {
-        if ( err ) {
-            return res.status(400).json({
-                msg: err
-            });
-        }
-
-        res.status(200).json({
-            payload: result
-        });
+    res.status(200).json({
+      payload: result,
     });
-}
+  });
+};
 
-export const putContacto = (req: Request, res: Response ) =>  {
+export const putContacto = (req: Request, res: Response) => {
+  const { idContacto } = req.params;
+  const { body } = req;
 
-    const { idContacto } = req.params;
-    const { body } = req;
-    
-    getContactoByID(Number(idContacto))
-        .then( (oldContacto: any) => {
+  getContactoByID(Number(idContacto))
+    .then((oldContacto: any) => {
+      const nombre = body.nombre || oldContacto[0].nombre;
+      const puesto = body.puesto || oldContacto[0].puesto;
+      const email = body.email || oldContacto[0].email;
+      const telefono = body.telefono || oldContacto[0].telefono;
+      const web = body.web || oldContacto[0].web;
+      const skype = body.skype || oldContacto[0].skype;
+      const twiter = body.twiter || oldContacto[0].twiter;
+      const facebook = body.facebook || oldContacto[0].facebook;
+      const instagram = body.instagram || oldContacto[0].instagram;
 
-            const nombre    = body.nombre    || oldContacto[0].nombre;
-            const puesto    = body.puesto    || oldContacto[0].puesto;
-            const email     = body.email     || oldContacto[0].email;
-            const telefono  = body.telefono  || oldContacto[0].telefono;
-            const web       = body.web       || oldContacto[0].web;
-            const skype     = body.skype     || oldContacto[0].skype;
-            const twiter    = body.twiter    || oldContacto[0].twiter;
-            const facebook  = body.facebook  || oldContacto[0].facebook;
-            const instagram = body.instagram || oldContacto[0].instagram;
-
-            
-            const query = `UPDATE contactos SET nombre = ?, puesto =  ?, email =  ?, telefono =  ?, web =  ?, skype =  ?
+      const query = `UPDATE contactos SET nombre = ?, puesto =  ?, email =  ?, telefono =  ?, web =  ?, skype =  ?
                             twiter =  ?, facebook =  ?, instagram =  ? WHERE _id = ${idContacto}`;
-            const campos = [nombre, puesto, email, telefono, web, skype, twiter, facebook, instagram];
-            MySql.ejecutarQuery(query, campos, (err: any, result: any) => {
-                if (err) {
-                    return res.status(400).json( {
-                        msg: 'Error en la actualización: ' + err ,
-                        ok: true,
-                        idContacto,
-                        body
-                    } );
-                }
-
-                return res.status(200).json({
-                    msj: 'Se ha actualizado el contacto',
-                    result
-                });
-
-            });
-        })
-        .catch ((err: any )=> {
-            res.status(400).json( {
-                msg: 'Error en la actualización: ' + err ,
-                ok: true,
-                idContacto,
-                body
-            } );
-        });
-
-   
-}
-
-export const deleteContacto = (req: Request, res: Response ) =>  {
-    
-    const { idContacto } = req.params;
-
-    const query = `DELETE FROM contactos WHERE _id = ${idContacto}`;
-
-    MySql.ejecutarQuery(query, [], (err: any, result: any) => {
-        if ( err ) {
-            return res.status(400).json({
-                msg: err
-            });
+      const campos = [nombre, puesto, email, telefono, web, skype, twiter, facebook, instagram];
+      MySql.ejecutarQuery(query, campos, (err: any, result: any) => {
+        if (err) {
+          return res.status(400).json({
+            msg: "Error en la actualización: " + err,
+            ok: true,
+            idContacto,
+            body,
+          });
         }
 
-        res.status(200).json({
-            payload: result
+        return res.status(200).json({
+          msj: "Se ha actualizado el contacto",
+          result,
         });
+      });
+    })
+    .catch((err: any) => {
+      res.status(400).json({
+        msg: "Error en la actualización: " + err,
+        ok: true,
+        idContacto,
+        body,
+      });
     });
-}
+};
+
+export const deleteContacto = (req: Request, res: Response) => {
+  const { idContacto } = req.params;
+
+  const query = `DELETE FROM contactos WHERE _id = ${idContacto}`;
+
+  MySql.ejecutarQuery(query, [], (err: any, result: any) => {
+    if (err) {
+      return res.status(400).json({
+        msg: err,
+      });
+    }
+
+    res.status(200).json({
+      payload: result,
+    });
+  });
+};
 
 const getContactoByID = async (id: number) => {
-
-    return new Promise( (resolve, reject) => {
-        const query = `SELECT * FROM contactos WHERE _id = ${id}`;
-        MySql.ejecutarQuery(query, [], (err: any, contacto: any) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(contacto);
-        });
+  return new Promise((resolve, reject) => {
+    const query = `SELECT * FROM contactos WHERE _id = ${id}`;
+    MySql.ejecutarQuery(query, [], (err: any, contacto: any) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(contacto);
     });
-}
+  });
+};
--- src/controllers/usuarios.controller.ts
+++ src/controllers/usuarios.controller.ts
@@ -1,183 +1,162 @@
-import { Request, Response } from 'express';
-import MySql from '../db/mysql';
-import md5 from 'md5';
-import path from 'path';
-import fs from 'fs';
+import { Request, Response } from "express";
+import MySql from "../db/mysql";
+import md5 from "md5";
+import path from "path";
+import fs from "fs";
 
 export const getUsuarios = (req: Request, res: Response) => {
+  const query = "SELECT * FROM usuarios";
 
-    const query = 'SELECT * FROM usuarios';
+  MySql.ejecutarQuery(query, [], (err: any, usuarios: any) => {
+    if (err) {
+      return res.status(400).json({
+        msg: err,
+      });
+    }
 
-    MySql.ejecutarQuery(query, [], (err: any, usuarios: any) => {
-        if (err) {
-            return res.status(400).json({
-                msg: err
-            });
-        }
-
-        res.status(200).json({
-            payload: usuarios
-        });
+    res.status(200).json({
+      payload: usuarios,
     });
-}
+  });
+};
 
 export const getUsuario = (req: Request, res: Response) => {
+  const { id } = req.params;
 
-    const { id } = req.params;
+  const query = `SELECT * FROM usuarios WHERE _id = ${id} LIMIT 1`;
 
-    const query = `SELECT * FROM usuarios WHERE _id = ${id} LIMIT 1`;
+  MySql.ejecutarQuery(query, [], (err: any, usuario: any) => {
+    if (err) {
+      return res.status(400).json({
+        msg: err,
+      });
+    }
 
-    MySql.ejecutarQuery(query, [], (err: any, usuario: any) => {
-        if (err) {
-            return res.status(400).json({
-                msg: err
-            });
-        }
-
-        res.status(200).json({
-            payload: usuario
-        });
+    res.status(200).json({
+      payload: usuario,
     });
-}
+  });
+};
 
 export const postUsuario = (req: Request, res: Response) => {
+  const { body } = req;
 
-    const { body } = req;
+  // encriptar la password para que coincida
+  body.password = md5(body.password);
 
-    // encriptar la password para que coincida
-    body.password = md5(body.password);
+  const query = "INSERT INTO usuarios (nombre, email, password, contenedor_id, role, telefono) VALUES (?, ?, ?, ?, ?, ?)";
+  const campos = [body.nombre, body.email, body.password, body.contenedor_id, body.role, body.telefono];
+  MySql.ejecutarQuery(query, campos, (err: any, result: any) => {
+    if (err) {
+      return res.status(400).json({
+        msg: err,
+      });
+    }
 
-    const query = 'INSERT INTO usuarios (nombre, email, password, contenedor_id, role, telefono) VALUES (?, ?, ?, ?, ?, ?)';
-    const campos = [body.nombre, body.email, body.password, body.contenedor_id, body.role, body.telefono]
-    MySql.ejecutarQuery(query, campos, (err: any, result: any) => {
-        if (err) {
-            return res.status(400).json({
-                msg: err
-            });
-        }
-
-        res.status(200).json({
-            payload: result
-        });
+    res.status(200).json({
+      payload: result,
     });
-}
+  });
+};
 
 export const putUsuario = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { body } = req;
 
-    const { id } = req.params;
-    const { body } = req;
+  getUsuarioById(Number(id))
+    .then((oldUser: any) => {
+      const nombre = body.nombre || oldUser[0].nombre;
+      const role = body.role || oldUser[0].role;
+      const telefono = body.telefono || oldUser[0].telefono;
 
-    getUsuarioById(Number(id))
-        .then((oldUser: any) => {
+      const query = `UPDATE usuarios SET nombre =  '${nombre}', role =  '${role}', telefono =  '${telefono}' WHERE _id = ${id}`;
 
-            const nombre = body.nombre || oldUser[0].nombre;
-            const role = body.role || oldUser[0].role;
-            const telefono = body.telefono || oldUser[0].telefono;
-
-            const query = `UPDATE usuarios SET nombre =  '${nombre}', role =  '${role}', telefono =  '${telefono}' WHERE _id = ${id}`;
-
-            MySql.ejecutarQuery(query, [], (err: any, result: any) => {
-                if (err) {
-                    return res.status(400).json({
-                        msg: 'Error en la actualización: ' + err,
-                        ok: true,
-                        id,
-                        body
-                    });
-                }
-
-                return res.status(200).json({
-                    msj: 'Se ha actualizado el usuario'
-                });
-
-            });
-        })
-        .catch(err => {
-            res.status(400).json({
-                msg: 'Error en la actualización: ' + err,
-                ok: true,
-                id,
-                body
-            });
-        });
-
-
-}
-
-export const deleteUsuario = (req: Request, res: Response) => {
-
-    const { id } = req.params;
-
-    const query = `DELETE FROM usuarios WHERE _id = ${id}`;
-
-    MySql.ejecutarQuery(query, [], (err: any, result: any) => {
+      MySql.ejecutarQuery(query, [], (err: any, result: any) => {
         if (err) {
-            return res.status(400).json({
-                msg: err
-            });
+          return res.status(400).json({
+            msg: "Error en la actualización: " + err,
+            ok: true,
+            id,
+            body,
+          });
         }
 
-        res.status(200).json({
-            payload: result
+        return res.status(200).json({
+          msj: "Se ha actualizado el usuario",
         });
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        msg: "Error en la actualización: " + err,
+        ok: true,
+        id,
+        body,
+      });
     });
-}
+};
+
+export const deleteUsuario = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const query = `DELETE FROM usuarios WHERE _id = ${id}`;
+
+  MySql.ejecutarQuery(query, [], (err: any, result: any) => {
+    if (err) {
+      return res.status(400).json({
+        msg: err,
+      });
+    }
+
+    res.status(200).json({
+      payload: result,
+    });
+  });
+};
 
 export const actualizarAvatar = (id: number, img: string) => {
+  eliminarAvatar(id)
+    .then(() => {
+      const query = `UPDATE usuarios SET img = '${img}' WHERE _id = ${id}`;
 
-    eliminarAvatar(id)
-        .then(() => {
-
-            const query = `UPDATE usuarios SET img = '${img}' WHERE _id = ${id}`;
-
-            MySql.ejecutarQuery(query, [], (err: any, result: any) => {
-                if (err) {
-                    return (err);
-                }
-                return (result[0]);
-
-            });
-        })
-        .catch(err => {
-            return (err)
-        });
-
-
-}
+      MySql.ejecutarQuery(query, [], (err: any, result: any) => {
+        if (err) {
+          return err;
+        }
+        return result[0];
+      });
+    })
+    .catch((err) => {
+      return err;
+    });
+};
 
 const eliminarAvatar = (id: number) => {
-
-    return new Promise((resolve, reject) => {
-
-        getUsuarioById(id)
-            .then((usuario: any) => {
-
-                const pathAntiguo = path.resolve(__dirname, '../assets/uploads/avatar/', usuario[0].img);
-                if (fs.existsSync(pathAntiguo)) {
-                    fs.unlink(pathAntiguo, (error) => {
-                        reject(error)
-                    });
-                }
-                resolve(true)
-
-            })
-            .catch(error => {
-                reject(error)
-            });
-    })
-
-}
+  return new Promise((resolve, reject) => {
+    getUsuarioById(id)
+      .then((usuario: any) => {
+        const pathAntiguo = path.resolve(__dirname, "../assets/uploads/avatar/", usuario[0].img);
+        if (fs.existsSync(pathAntiguo)) {
+          fs.unlink(pathAntiguo, (error) => {
+            reject(error);
+          });
+        }
+        resolve(true);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
 
 const getUsuarioById = async (id: number) => {
-
-    return new Promise((resolve, reject) => {
-        const query = `SELECT * FROM usuarios WHERE _id = ${id}`;
-        MySql.ejecutarQuery(query, [], (err: any, usuario: any) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(usuario);
-        });
+  return new Promise((resolve, reject) => {
+    const query = `SELECT * FROM usuarios WHERE _id = ${id}`;
+    MySql.ejecutarQuery(query, [], (err: any, usuario: any) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(usuario);
     });
-}
-
+  });
+};
