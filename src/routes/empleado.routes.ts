8a39b1e98b4674f6f0b85ccfa2673f6b3c1fcd88
Gabriel <gjulian.dev@gmail.com>
2025-01-12 23:10:29 +0100

    fix ausencais

--- src/routes/empleado.routes.ts
+++ src/routes/empleado.routes.ts
@@ -11,17 +11,18 @@ import {
   getContadoresByBolsaEmpleadoAndTipo,
   getUnidadesConsumidasByEmpleado,
   getUnidadesConsumidas,
+  getAusenciasByEmpleado,
 } from "../controllers/empleados.controller";
 
 const routes = Router();
 
+routes.get("/ausencias/:empleado_id", getAusenciasByEmpleado);
 routes.get("/contadores/:empleado_id", getContadoresByEmpleado); 
 routes.get("/bolsa-horas-empleado/:empleado_id/", getContadoresByBolsaEmpleado);
 routes.get("/:empleado_id/bolsa-horas-empleado-tipo/:tipo_id/", getContadoresByBolsaEmpleadoAndTipo);
 routes.get("/:empleado_id/unidades-by-solicitud/:tipo_solicitud", getNumUnidadesBySolicitud);
 routes.get("/unidades-disfrutadas-by-empleado/:empleado_id", getUnidadesConsumidasByEmpleado);
 routes.get("/unidades-disfrutadas/", getUnidadesConsumidas);
-
 routes.get("/contenedor/:contenedor_id", getEmpleadosByContenedor);
 routes.get("/:empleado_id", getEmpleado);
 routes.post("/", postEmpleado);
