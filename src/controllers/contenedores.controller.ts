f2fc594d99cd2e259a487c31135f6db70d2e2780
Gabriel <gjulian.dev@gmail.com>
2025-01-13 15:05:58 +0100

    fix aniocaducidad

--- src/controllers/contenedores.controller.ts
+++ src/controllers/contenedores.controller.ts
@@ -106,8 +106,11 @@ export const postTipoSolicitud = (req: Request, res: Response) => {
   const { contenedor_id } = req.params;
   const { body } = req;
 
-  const query = `INSERT INTO aux_tipo_solicitud (contenedor_id, nombre, alias, unidades, unidad_medida, dia_caducidad, mes_caducidad, anio_caducidad, esBolsa) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-  const campos = [body.contenedor_id, body.nombre, body.alias, body.unidades, body.unidad_medida, body.dia_caducidad, body.mes_caducidad, body.anio_caducidad, body.esBolsa];
+  const query = `INSERT INTO aux_tipo_solicitud (contenedor_id, nombre, alias, unidades, unidad_medida, 
+                  dia_caducidad, mes_caducidad, anio_caducidad, esBolsa) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const campos = [body.contenedor_id, body.nombre, body.alias, body.unidades, body.unidad_medida, 
+                  body.dia_caducidad, body.mes_caducidad, body.anio_caducidad, body.esBolsa];
 
   MySql.ejecutarQuery(query, campos, (err: any, result: any) => {
     if (err) {
