8de5acf40d49e390de1ba4db834000f0c6bbbf39
Gabriel <gjulian.dev@gmail.com>
2025-04-25 10:54:29 +0200

    fix pre

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import Server from './server/server';
 
+//produccion
+// const server = new Server(13007);
 
-const server = new Server(13007);
+//preproducion
+const server = new Server(13014);
 
 //server.startHttp();
 server.startHttps();
